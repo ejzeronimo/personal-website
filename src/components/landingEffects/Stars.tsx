d357beb536a5227a1542fb119d63fffab0c9b638
Ethan Zeronik <ejzeronik@gmail.com>
2025-01-24 23:39:00 -0500

    mixed approach of r3f and threejs, is identical now

--- src/components/landingEffects/Stars.tsx
+++ src/components/landingEffects/Stars.tsx
@@ -1,50 +1,65 @@
-import React, { useState, useEffect, useRef, useLayoutEffect } from 'react';
-import { Canvas, useFrame, useThree } from '@react-three/fiber'
-import { CameraControls, OrbitControls, PerspectiveCamera, PointMaterial, Points } from '@react-three/drei';
+import React, { useState, useEffect, useRef } from 'react';
+import { Canvas, useFrame } from '@react-three/fiber'
+import { PerspectiveCamera, Points } from '@react-three/drei';
 import * as THREE from 'three';
-
-const pointCount = 12000;
-const boxSize = 600;
-
-const points = Array.from({ length: pointCount }, () => new THREE.Vector3(Math.random() * boxSize - (boxSize / 2), Math.random() * boxSize - (boxSize / 2), Math.random() * boxSize - (boxSize / 2)));
-
-const velocities = Array.from({ length: pointCount }, () => 0)
-const accelerations = Array.from({ length: pointCount }, () => Math.random() * (.001 - .0005) + .0005);
 
 function StarPointCloud() {
     const pointRef = useRef<THREE.Points>(null!);
 
-    useFrame((state, delta, xrFrame) => {
+    // HACK: I could not tell you why but this squashes the whole image to fit rather than the standard react-3-fiber approach that crops
+    useEffect(() => {
         if (pointRef.current) {
-            const starPoints = pointRef.current;
-            const positionAttribute = starPoints.geometry.getAttribute('position');
+            // make stars
+            let points: THREE.Vector3[] = [];
+            let velocities: number[] = [];
+            let accelerations: number[] = [];
 
-            for (let i = 0; i < positionAttribute.count; i++) {
-                let y = positionAttribute.getY(i);
+            for (let i = 0; i < 6000; i++) {
+                let star = new THREE.Vector3(
+                    Math.random() * 600 - 300,
+                    Math.random() * 600 - 300,
+                    Math.random() * 600 - 300
+                );
 
-                velocities[i] = velocities[i] + accelerations[i];
-                y -= velocities[i];
-
-                if (y < -200) {
-                    y = 200;
-                    velocities[i] = 0;
-                }
-
-                positionAttribute.setY(i, y);
+                points.push(star);
+                velocities.push(0);
+                accelerations.push(Math.random() * (.001 - .0005) + .0005);
             }
 
-            starPoints.rotation.y += 0.001;
-        }
-    });
+            let starBufferGeometry = new THREE.BufferGeometry().setFromPoints(points);
+            let stars = pointRef.current;
+            stars.geometry = starBufferGeometry;
 
-    let foo = points.map((vec) => {
-        return vec.x, vec.y, vec.z
-    });
+            function animate() {
+                const positionAttribute = starBufferGeometry.getAttribute('position');
+
+                for (let i = 0; i < positionAttribute.count; i++) {
+                    let y = positionAttribute.getY(i);
+
+                    velocities[i] = velocities[i] + accelerations[i];
+                    y -= velocities[i];
+
+                    if (y < -200) {
+                        y = 200;
+                        velocities[i] = 0;
+                    }
+
+                    positionAttribute.setY(i, y);
+                }
+
+                positionAttribute.needsUpdate = true;
+                stars.rotation.y += 0.001;
+
+                requestAnimationFrame(animate);
+            }
+
+            animate();
+        }
+    }, []);
 
     return (
-        <Points positions={new Float32Array(foo)} ref={pointRef}>
+        <Points positions={new Float32Array()} ref={pointRef}>
             <pointsMaterial
-                color={new THREE.Color(0xffffff)}
                 size={0.02}
                 sizeAttenuation={true}
                 depthWrite={false}
@@ -56,6 +71,7 @@ function StarPointCloud() {
 
 export default function LandingEffectController() {
     const [windowSize, setWindowSize] = useState({ width: window.innerWidth, height: window.innerHeight });
+    const cameraRef = useRef<THREE.PerspectiveCamera>(null!);
 
     useEffect(() => {
         const controller = new AbortController();
@@ -75,14 +91,14 @@ export default function LandingEffectController() {
         <Canvas
             className="absolute left-0 top-0 z-[2] h-full w-full"
             gl={canvas => {
-                let foo = new THREE.WebGLRenderer({ canvas, alpha: true });
-                foo.setSize(window.innerWidth, window.innerHeight);
+                let renderer = new THREE.WebGLRenderer({ canvas, antialias: false, alpha: true, powerPreference: "default" });
+                renderer.toneMapping = THREE.NoToneMapping;
+                renderer.setSize(window.innerWidth, window.innerHeight);
 
-                return foo;
+                return renderer;
             }}>
-            <PerspectiveCamera makeDefault aspect={windowSize.width / windowSize.height} position={[0, 0, 1]} rotation={[Math.PI / 2, 0, 0]} fov={60} near={1} far={1000} />
+            <PerspectiveCamera makeDefault aspect={windowSize.width / windowSize.height} position={[0, 1, 0]} rotation={[Math.PI / 2, 0, 0]} fov={60} near={1} far={1000} ref={cameraRef} />
             <StarPointCloud />
-            {/* <CameraControls /> */}
         </Canvas>
     );
 }
